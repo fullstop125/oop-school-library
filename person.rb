d8127caaf6ca33c8e847fe7ffb13e2929df2fe5e
fullstop125 <hassanmoses125@gmail.com>
2022-11-03 21:34:53 +0300

    decorate class

--- person.rb
+++ person.rb
@@ -1,12 +1,26 @@
-class Person
+require './nameable'
+require './decorator'
+require './trimmerdecorator'
+require './capitalizedecorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -14,8 +28,11 @@ class Person
   def of_age?
     @age >= 18
   end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
